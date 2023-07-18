ee4cfef7af1dadbcd4f2a0da61070b0dff7c1b26
siglog <siglogtms@gmail.com>
2023-07-18 21:50:35 +0100

    feat(Typescript): removing to solve issue
    It was requiring package.lock file to run the make command

--- makefiles/grpc.mk
+++ makefiles/grpc.mk
@@ -1,4 +1,4 @@
-src/grpc-bridge/welsh-clients.gen.ts: src/grpc-bridge/gen-clients.js src/api/protocoltypes.pb.js node_modules
+src/grpc-bridge/welsh-clients.gen.ts: src/grpc-bridge/gen-clients.js src/api/protocoltypes.pb.js
 	$(call check-program, npx)
 	npx babel-node src/grpc-bridge/gen-clients.js > $@
 	npx eslint --no-ignore --fix $@
